98366b1a46e241be12b3492ae878b40441e90afb
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2023-04-21 23:01:01 +0530

    Update led.ino

--- led.ino
+++ led.ino
@@ -1,10 +1,12 @@
+
 void setup() {
   // put your setup code here, to run once:
-    
+  pinMode(13,OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED,HIGH);
+  delay(1000);
   
 }
